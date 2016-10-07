59680352af99368b3a2b0bab4171cf891e80135f
Victor <xiwang2000@gmail.com>
2016-10-07 20:01:35 +0800

    #7 add i18n config

--- src/Module.php
+++ src/Module.php
@@ -16,4 +16,16 @@ class Module extends \yii\base\Module
      * @var string
      */
     public $layout = 'main';
+    
+    public function init()
+    {
+        parent::init();
+        if (!isset(Yii::$app->i18n->translations['workflow'])) {
+            Yii::$app->i18n->translations['workflow'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath' => '@cornernote/workflow/manager/message'
+            ];
+        }
+    }
 }
